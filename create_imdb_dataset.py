e6f2e9db3a1d788aa7c7b67644647948d069fcee
Mathis Chenuet <mathis.chenuet@unchartech.com>
2018-11-02 19:22:29 +0100

    wip imdb dataset creation

---
+++ create_imdb_dataset.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+from datetime import time as dt_time
+from datetime import timedelta
+from pathlib import Path
+from sys import stderr
+from typing import Dict, Optional
+from zipfile import ZipFile
+
+import attr
+import dateutil.parser
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+
+
+def main(source, target):
+    source = Path(source).expanduser()
+    target = Path(target).expanduser()
+    target.mkdir(parents=True, exist_ok=True)
+
+    CLASSES = ["neg", "pos"]
+
+    def get_texts(path):
+        texts, labels = [], []
+        it = (
+            (idx, fname)
+            for idx, label in enumerate(CLASSES)
+            for fname in (path / label).glob("*.txt")
+        )
+        for idx, fname in tqdm(it, desc=f"Loading {path}", ncols=80, position=0):
+            texts.append(fname.read_text(encoding="utf-8"))
+            labels.append(idx)
+        return pd.DataFrame({"label": labels, "text": texts})
+
+    train = get_texts(source / "train")
+    test = get_texts(source / "test")
+
+    train, dev = train_test_split(train, test_size=.1, random_state=42)
+
+    train.to_csv(target / "train.tsv", "\t", index=False)
+    dev.to_csv(target / "dev.tsv", "\t", index=False)
+    test.to_csv(target / "test.tsv", "\t", index=False)
+
+
+if __name__ == "__main__":
+    main(source="~/Documents/.data/imdb/aclImdb", target="~/.data/glue_data/IMDB")
