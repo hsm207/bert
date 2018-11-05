7fcc2e3255ff9fe385987c91f8c6e5631969c8d8
Mathis Chenuet <mathis.chenuet@unchartech.com>
2018-11-05 18:13:12 +0100

    update script for AWS

--- train_imdb.sh
+++ train_imdb.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-BERT_BASE_DIR='checkpoints/uncased_L-12_H-768_A-12'
-GLUE_DIR="$HOME/.data/glue_data"
+# BERT_BASE_DIR='checkpoints/uncased_L-12_H-768_A-12'
+BERT_BASE_DIR='/data/checkpoints/uncased_L-24_H-1024_A-16'
+# DATASETS="$HOME/.data/glue_data"
+DATASETS='/data/datasets'
+
+tensorboard --logdir /data/outputs &
 
 python run_classifier.py \
   --task_name=IMDB \
   --do_train=true \
   --do_eval=true \
-  --data_dir=$GLUE_DIR/IMDB \
+  --data_dir=$DATASETS/IMDB \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
@@ -15,5 +19,5 @@ python run_classifier.py \
   --train_batch_size=32 \
   --learning_rate=2e-5 \
   --num_train_epochs=3 \
-  --output_dir=/tmp/mrpc_output/ \
+  --output_dir=/data/outputs/imdb \
   $@
